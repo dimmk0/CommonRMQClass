b40a57cd3f9233e214b3f00b0797a38324395945
Dmytro Kaliazin <dkaliazi@cisco.com>
2019-08-02 13:48:15 +0300

    Add init, setup and consume methods.

--- CommonRMQ.py
+++ CommonRMQ.py
@@ -1,14 +1,42 @@
 
+import pika
+
 
 class CommonRMQ(object):
-""
+    """Class to work with queues"""
     def __init__(self):
-        pass
+        self.queue = 'test_output_queue'
+        self.rmq_server = 'localhost'
+        self.expected_message_count = 1
+        self.message_count = 0
 
-    def connect_rmq(self):
-        pass
+        self.credentials = pika.PlainCredentials('admin',
+                                                 '')
+        self._setup_queue()
+        print("INit done")
 
-    def get_message(self):
+    def _setup_queue(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(self.rmq_server, credentials=self.credentials))
+
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue)
+        self.tag = self.channel.basic_consume(queue=self.queue,
+                                              auto_ack=True,
+                                              on_message_callback=self.callback)
+
+    def start_consume(self):
+        self.channel.start_consuming()
+
+    def callback(self, ch, method, properties, body):
+        print(" [x] Received %r" % body)
+        if self.message_count >= self.expected_message_count:
+            self.connection.close()
+            print("Consume is done")
+        else:
+            self.message_count += 1
+
+    def send_message(self):
         pass
 
     def save_message(self):
@@ -18,4 +46,7 @@ class CommonRMQ(object):
         pass
 
 
+if __name__ == "__main__":
 
+    rmq = CommonRMQ()
+    rmq.start_consume()
