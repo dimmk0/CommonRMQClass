eeacfb5b93ac7147b47cafe23d445c6640640a51
Dmytro Kaliazin <dkaliazi@cisco.com>
2019-08-02 11:23:14 +0300

    Initial commit

---
+++ CommonRMQ.py
@@ -0,0 +1,21 @@
+
+
+class CommonRMQ(object):
+""
+    def __init__(self):
+        pass
+
+    def connect_rmq(self):
+        pass
+
+    def get_message(self):
+        pass
+
+    def save_message(self):
+        pass
+
+    def print_message(self):
+        pass
+
+
+
